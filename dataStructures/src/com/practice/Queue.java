fd1d19501aacafd3ddf8cb2febbc799d85a2253d
abhimanyunarwal <anarwal.27@gmail.com>
2017-01-31 23:17:55 -0700

    Queues and Stack as array

---
+++ dataStructures/src/com/practice/Queue.java
@@ -0,0 +1,75 @@
+package com.practice;
+
+import java.util.Arrays;
+
+/**
+ * Created by abhimanyunarwal on 1/31/17.
+ */
+public class Queue {
+    private int front;
+    private int rear;
+    int size;
+    int[] queue;
+
+    public Queue(int inputSize) {
+        if(inputSize<0){
+            System.out.println("Negative values not allowed");
+        }
+        front=-1;
+        rear=-1;
+        size = inputSize;
+        queue = new int[inputSize];
+    }
+
+    public boolean isEmpty() {
+        if (front == -1 && rear == -1) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public void enqueue(int element) {
+
+        if ((front - rear + 1) == size) {
+            System.out.println("Sorry the queue is already full");
+        } else if(isEmpty()){
+            front++;
+            rear++;
+            queue[rear]=element;
+        }
+        else{
+            rear=(rear+1)%size;
+            queue[rear]=element;
+        }
+    }
+
+    public int dequeue(){
+        int value=0;
+        if(isEmpty()){
+            System.out.println("The queue is already empty, nothing to delete");
+        }
+        else if(front==rear){
+            System.out.println("There is just one element in queue");
+            value=queue[front];
+            front--;
+            rear--;
+        }else{
+            value=queue[front];
+            front=(front+1)%size;
+        }
+        return value;
+    }
+
+    public void display(){
+        System.out.println(Arrays.toString(queue));
+    }
+
+    public static void main(String[] args) {
+        Queue newQueue= new Queue(5);
+         newQueue.enqueue(4);
+         newQueue.enqueue(3);
+         newQueue.enqueue(7);
+         newQueue.display();
+    }
+}
---
+++ dataStructures/src/com/practice/Stack.java
@@ -0,0 +1,46 @@
+package com.practice;
+
+/**
+ * Created by abhimanyunarwal on 1/31/17.
+ */
+public class Stack{
+    int size;
+    int[] stackArray;
+    int top;
+
+    public Stack(int s){
+        size = s;
+        stackArray = new int[size];
+        top = -1;
+    }
+
+    public int push (int num){return stackArray[++top]=num;}
+    public int pop (){return stackArray[top--];}
+    public int peek (){return stackArray[top];}
+    public boolean isFull(){if(top==size-1){return true;}return false;}
+
+    public static void main(String[] args){
+        Stack stack=new Stack(5);
+        stack.push(4);
+        stack.push(2);
+        stack.push(1);
+        stack.push(3);
+        stack.push(6);
+
+
+        System.out.println("Stack's current size is: "+stack.size);
+        if(!stack.isFull()){
+        stack.push(6);
+        }
+        else{
+        System.out.println("Stack is full, let me delete an element, poping: "+stack.peek());
+        stack.pop();
+        System.out.println("Popping one more: "+stack.peek());
+        stack.pop();
+        System.out.println("Stack's top has been popped, now adding");
+        stack.push(6);
+        }
+        }
+
+
+}
