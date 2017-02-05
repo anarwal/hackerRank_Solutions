554266a8272bb3ba5cdc82e2225fcd48d9a8464b
abhimanyunarwal <anarwal.27@gmail.com>
2017-02-04 17:24:14 -0700

    Renamed Files

--- dataStructures/src/com/dataStructures/StackProgram.java
+++ dataStructures/src/com/dataStructures/StackProgram.java
@@ -4,12 +4,12 @@ package com.dataStructures;
  * Created by abhimanyunarwal on 1/31/17.
  * implement stack (LIFO) using array
  */
-public class Stack{
+public class StackProgram {
     int size;
     int[] stackArray;
     int top;
 
-    public Stack(int s){
+    public StackProgram(int s){
         size = s;
         stackArray = new int[size];
         top = -1;
@@ -21,7 +21,7 @@ public class Stack{
     public boolean isFull(){if(top==size-1){return true;}return false;}
 
     public static void main(String[] args){
-        Stack stack=new Stack(5);
+        StackProgram stack=new StackProgram(5);
         stack.push(4);
         stack.push(2);
         stack.push(1);
@@ -29,16 +29,16 @@ public class Stack{
         stack.push(6);
 
 
-        System.out.println("Stack's current size is: "+stack.size);
+        System.out.println("StackProgram's current size is: "+stack.size);
         if(!stack.isFull()){
         stack.push(6);
         }
         else{
-        System.out.println("Stack is full, let me delete an element, poping: "+stack.peek());
+        System.out.println("StackProgram is full, let me delete an element, poping: "+stack.peek());
         stack.pop();
         System.out.println("Popping one more: "+stack.peek());
         stack.pop();
-        System.out.println("Stack's top has been popped, now adding");
+        System.out.println("StackProgram's top has been popped, now adding");
         stack.push(6);
         }
         }
